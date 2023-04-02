e09c398e914e7ed2fb350a02cc0f40cd8534bdef
SHL4917 <siewhan96@gmail.com>
2023-04-02 17:06:06 +0800

    UI and ancillary logic cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { getFunctions, httpsCallable, connectFunctionsEmulator } from 'firebase/functions'
+import {
+  getFunctions,
+  httpsCallable,
+  connectFunctionsEmulator,
+} from "firebase/functions";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {useForm} from 'react-hook-form'
+import { useForm } from "react-hook-form";
 import { useState } from "react";
+import SendIcon from "./icons/SendIcon";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAgby7hOBRU-eoAXNvJ53MoVOzOe1TBEsE",
@@ -23,41 +28,81 @@ const functions = getFunctions(app);
 
 const hostname = window.location.hostname;
 
-if (hostname === 'localhost') {
-  connectFunctionsEmulator(functions, 'localhost', 5001)
+if (hostname === "localhost") {
+  connectFunctionsEmulator(functions, "localhost", 5001);
 }
 
 function App() {
-  const [answer, setAnswer] = useState("")
+  const [answer, setAnswer] = useState("");
+  const [question, setQuestion] = useState("");
+
   const submitQuestion = (formData) => {
-    const submitQ = httpsCallable(functions, 'submitQuestion');
-    submitQ({question: formData.question}).then((result) =>{
-      // TODO some error handling
-      console.log(result)
-      setAnswer(result.data)
-    })
-  }
+    setAnswer("Submitting question, please hold on...");
+    setQuestion(formData.question)
+    const submitQ = httpsCallable(functions, "submitQuestion");
+    submitQ({ question: formData.question })
+      .then((result) => {
+        console.log(result);
+        setAnswer(result.data);
+      })
+      .catch((err) => {
+        setAnswer(
+          "I'm sorry, but there appears to be an error. Please try again"
+        );
+      });
+  };
 
   const {
     register,
     handleSubmit,
-    formState: {errors}
+    formState: { errors },
   } = useForm();
 
+  const handleKeypress = (e) => {
+    e.target.style.height = 'inherit';
+    e.target.style.height = `${Math.min(e.target.scrollHeight, 300)}px`;
+    if(e.which === 13 && !e.shiftKey) {
+      e.preventDefault();
+  
+      handleSubmit(submitQuestion)();
+    }
+  }
+
   return (
-    <div>
-      <div>Hello!</div>
-      <form onSubmit={handleSubmit(submitQuestion)}>
-        <div>
-          <label>Question</label>
-          <input type="text" name="question" {...register("question")} />
-        </div>
-        <div>
-          <label></label>
-          <button type="submit">Login</button>
-        </div>
+    <div class="flex flex-col min-h-screen">
+      <div class="flex flex-col">
+        <div>Hello!</div>
+        <div class="whitespace-pre-line">{question}</div>
+        <div class="whitespace-pre-line">{answer}</div>
+      </div>
+      <div class="grow h-full"></div>
+      <form
+        class="flex items-center py-2 px-3 bg-gray-100 rounded-lg dark:bg-gray-700"
+        onSubmit={handleSubmit(submitQuestion)}
+      >
+        <textarea
+          rows="1"
+          type="text"
+          name="question"
+          class="block mx-4 p-2.5 w-full text-gray-900 bg-white rounded-md border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Ask away!"
+          onKeyDown={handleKeypress}
+          {...register("question")}
+        ></textarea>
+        <button
+          type="submit"
+          class="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600"
+        >
+          <svg
+            class="w-6 h-6 rotate-90"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+          </svg>
+        </button>
       </form>
-      <div>{answer}</div>
     </div>
   );
 }
